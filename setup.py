df3f92da6ecbde4cf632331ed35843ac19f7a93a
Alex Kanitz <alexander.kanitz@alumni.ethz.ch>
2022-11-08 13:07:47 +0100

    build: update package setup config

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+"""Package setup."""
+
 from pathlib import Path
 from setuptools import (setup, find_packages)
 
@@ -5,24 +7,27 @@ root_dir = Path(__file__).parent.resolve()
 
 exec(open(root_dir / "pro_tes" / "version.py").read())
 
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
+file_name = root_dir / "README.md"
+with open(file_name, 'r') as _file:
+    long_description = _file.read()
+
+install_requires = []
+req = root_dir / 'requirements.txt'
+with open(req, "r") as _file:
+    install_requires = _file.read().splitlines()
 
 setup(
     name='pro-tes',
+    license='Apache License 2.0',
     version=__version__,  # noqa: F821
-    author='ELIXIR-Europe',
-    author_email='alexander.kanitz@alumni.ethz.ch',
-    description='Proxy GA4GH TES server',
+    description='Proxy/gateway GA4GH TES service',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    license='Apache License 2.0',
-    url='https://github.com/elixir-europe/proTES.git',
-    packages=find_packages(),
-    keywords=(
-        'ga4gh tes proxy rest restful api app server openapi '
-        'swagger python flask'
-    ),
+    url='https://github.com/elixir-cloud-aai/proTES',
+    author='Alexander Kanitz',
+    author_email='alexander.kanitz@alumni.ethz.ch',
+    maintainer='ELIXIR Cloud & AAI',
+    maintainer_email='cloud-service@elixir-europe.org',
     classifiers=[
         'License :: OSI Approved :: Apache Software License',
         'Development Status :: 3 - Alpha',
@@ -31,5 +36,19 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python :: 3.6',
     ],
-    install_requires=['connexion', 'Flask-Cors'],
+    keywords=(
+        'ga4gh tes proxy rest restful api app server openapi '
+        'swagger python flask'
+    ),
+    project_urls={
+        "Repository": "https://github.com/elixir-cloud-aai/proTES",
+        "ELIXIR Cloud & AAI": "https://elixir-cloud.dcc.sib.swiss/",
+        "Tracker": "https://github.com/elixir-cloud-aai/proTES/issues",
+    },
+    packages=find_packages(),
+    install_requires=install_requires,
+    setup_requires=[
+        "setuptools_git==1.2",
+        "twine==3.8.0"
+    ],
 )
